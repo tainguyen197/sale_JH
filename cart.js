5a410ebd5177f4760da02de43bb56f85e66e4342
tainguyen197 <tainguyen197.ntt@gmail.com>
2019-07-22 20:42:29 +0700

    final

--- cart.js
+++ cart.js
@@ -4,7 +4,6 @@ const listItem = [
     name: "Dress",
     describe: "Awn aywd gyawdgy wada awdv",
     price: {
-      imporPrice: 40000,
       sellPrice: 50000,
       tax: 0.15
     }
@@ -14,7 +13,6 @@ const listItem = [
     name: "Dress",
     describe: "Awn aywd gyawdgy wada awdv",
     price: {
-      imporPrice: 50000,
       sellPrice: 60000,
       tax: 0.1
     }
@@ -24,7 +22,6 @@ const listItem = [
     name: "Dress",
     describe: "Awn aywd gyawdgy wada awdv",
     price: {
-      imporPrice: 44000,
       sellPrice: 55000,
       tax: 0.15
     }
@@ -34,7 +31,6 @@ const listItem = [
     describe: "Awn aywd gyawdgy wada awdv",
     name: "Dress",
     price: {
-      imporPrice: 40000,
       sellPrice: 70000,
       tax: 0.15
     }
@@ -44,7 +40,6 @@ const listItem = [
     name: "Dress",
     describe: "Awn aywd gyawdgy wada awdv",
     price: {
-      imporPrice: 20000,
       sellPrice: 50000,
       tax: 0.15
     }
@@ -54,7 +49,6 @@ const listItem = [
     describe: "Awn aywd gyawdgy wada awdv",
     name: "Dress",
     price: {
-      imporPrice: 40000,
       sellPrice: 55000,
       tax: 0.15
     }
@@ -64,7 +58,6 @@ const listItem = [
     name: "Dress",
     describe: "Awn aywd gyawdgy wada awdv",
     price: {
-      imporPrice: 40000,
       sellPrice: 58000,
       tax: 0.15
     }
@@ -74,7 +67,6 @@ const listItem = [
     describe: "Awn aywd gyawdgy wada awdv",
     name: "Dress",
     price: {
-      imporPrice: 40000,
       sellPrice: 59000,
       tax: 0.15
     }
@@ -84,7 +76,6 @@ const listItem = [
     name: "Dress",
     describe: "Awn aywd gyawdgy wada awdv",
     price: {
-      imporPrice: 40000,
       sellPrice: 51000,
       tax: 0.15
     }
@@ -94,17 +85,15 @@ const listItem = [
     describe: "Awn aywd gyawdgy wada awdv",
     name: "Dress",
     price: {
-      imporPrice: 40000,
       sellPrice: 450000,
       tax: 0.15
     }
   },
   {
-    id:11,
+    id: 11,
     describe: "Awn aywd gyawdgy wada awdv",
     name: "Dress",
     price: {
-      imporPrice: 40000,
       sellPrice: 650000,
       tax: 0.15
     }
@@ -114,15 +103,32 @@ const listItem = [
     describe: "Awn aywd gyawdgy wada awdv",
     name: "Dress",
     price: {
-      imporPrice: 40000,
       sellPrice: 650000,
       tax: 0.15
     }
   }
 ];
+
+const totalPrice = {
+  totalPrice: {
+    receivePrice: 0,
+    sellPrice: 0,
+    discount: 0.15
+  },
+  commissionForProvider: {
+    percent: 0.12,
+    successfulCost: 10000
+  }
+};
 const listCart = [];
 const listMenu = document.getElementById("list-menu");
 listMenu.classList.add("list-menu");
+const menu = document.getElementById("menu");
+const nav = document.getElementById("nav-icon");
+const mainContent = document.getElementById("main-content");
+
+const listCartSaler = document.getElementById("list-cart");
+listCartSaler.classList.add("list-menu");
 
 const initItems = function() {
   const mainContain = document.getElementById("list-item");
@@ -172,56 +178,135 @@ const initItems = function() {
 };
 
 const isDuplicate = obj => {
-   return listCart.some(item => {
+  return listCart.some(item => {
     if (obj === item) return true;
   });
 };
 
-const updateNumberItem = (number, id) =>{
-  const inputElement =  document.querySelector('#input' + id);
-  inputElement.value = number;
-}
+updateNumberItem = (item, number) => {
+  if (item.number <= 1 && number < 0) {
+    listCart.some((itemCart, index) => {
+      if (itemCart.id === item.id) {
+        listCart.splice(index, 1);
+        return true;
+      }
+    });
+    const itemPro = document.getElementById(item.id);
+    const itemStore = document.getElementById("itemSaler" + item.id);
+    itemPro.classList.add("animation-item-box");
+    itemStore.classList.add("animation-item-box");
+    setTimeout(() => {
+      itemPro.remove();
+      itemStore.remove();
+    }, 750);
+    updatePriceItem();
+    return;
+  }
+  item.number += number;
+  //customer
+  const inputElement = document.querySelector("#input" + item.id);
+  const totalElement = document.querySelector("#total" + item.id);
+
+  inputElement.value = item.number;
+  totalElement.textContent =
+    item.number * item.price.sellPrice +
+    item.price.sellPrice * item.price.tax * item.number +
+    "(+" +
+    item.price.tax +
+    "% tax)";
+
+  //saler
+  const numberPro = document.querySelector("#number" + item.id);
+  const pricePro = document.querySelector("#pricePro" + item.id);
+
+  numberPro.textContent = item.number;
+  pricePro.textContent = item.number * item.price.sellPrice;
+  updatePriceItem();
+  // console.log(item.number*item.price.sellPrice + item.price.sellPrice*item.price.tax);
+};
+
+updatePriceItem = item => {
+  totalPrice.totalPrice.sellPrice = 0;
+  totalPrice.totalPrice.receivePrice = 0;
+
+  listCart.forEach(item => {
+    totalPrice.totalPrice.sellPrice +=
+      item.number * item.price.sellPrice +
+      item.price.tax * item.price.sellPrice * item.number;
+    totalPrice.totalPrice.receivePrice += item.number * item.price.sellPrice;
+  });
+
+  //customer
+  const totalMoney = document.querySelector("#moneyTotal");
+  const grossMoney = document.querySelector("#moneyGross");
+  const discount = document.querySelector("#discount");
+
+  //saler
+  const totalPricePro = document.querySelector("#moneyGrossSaler");
+  const commission = document.querySelector("#commission");
+  const successfullCost = document.querySelector("#successfulCost");
+  const moneySaler = document.querySelector("#moneySaler");
+
+  grossMoney.textContent = totalPrice.totalPrice.sellPrice;
+  discount.textContent = totalPrice.totalPrice.discount;
+  totalMoney.textContent =
+    totalPrice.totalPrice.sellPrice -
+    totalPrice.totalPrice.sellPrice * totalPrice.totalPrice.discount;
+
+  totalPricePro.textContent = totalPrice.totalPrice.receivePrice;
+  commission.textContent = totalPrice.commissionForProvider.percent;
+  successfullCost.textContent = totalPrice.commissionForProvider.successfulCost;
+  moneySaler.textContent =
+    totalPrice.totalPrice.receivePrice -
+    totalPrice.commissionForProvider.percent *
+      totalPrice.totalPrice.receivePrice -
+    totalPrice.commissionForProvider.successfulCost;
+};
 
 const addCart = item => {
- 
   //check is duplicate item
-  if(isDuplicate(item)){
-    item.number++;
-    updateNumberItem(item.number,item.id)
-
-  }
-  else{
+  if (isDuplicate(item)) {
+    updateNumberItem(item, 1);
+  } else {
     item.number = 1;
     listCart.push(item);
-   
+
+    // customer
     const numberInput = document.createElement("input");
-    numberInput.setAttribute('id','input'+ item.id)
+    numberInput.setAttribute("id", "input" + item.id);
+    numberInput.setAttribute("readonly", "true");
+
     const portfoliaMenu = document.createElement("div");
     portfoliaMenu.classList.add("item-box");
-    portfoliaMenu.setAttribute('id',item.id)
-  
+    portfoliaMenu.setAttribute("id", item.id);
+
     const titleMenu = document.createElement("h2");
     titleMenu.textContent = item.name;
-  
+
     const describeMenu = document.createElement("span");
     describeMenu.textContent = item.describe;
-  
-    
-    numberInput.value = '1';
+
+    numberInput.value = "1";
     const plusBtn = document.createElement("button");
     plusBtn.textContent = "+";
-  
+
     const abBtn = document.createElement("button");
     abBtn.textContent = "-";
-  
+
     const totalMenu = document.createElement("span");
     totalMenu.textContent = "Total";
-  
+
     const priceMenu = document.createElement("span");
-    priceMenu.textContent = item.price.sellPrice;
-  
+    priceMenu.setAttribute("id", "total" + item.id);
+    priceMenu.textContent =
+      item.number * item.price.sellPrice +
+      item.price.sellPrice * item.price.tax +
+      "(+" +
+      item.price.tax +
+      "% tax)";
+
     listMenu.appendChild(portfoliaMenu);
-  
+
     portfoliaMenu.appendChild(titleMenu);
     portfoliaMenu.appendChild(describeMenu);
     portfoliaMenu.appendChild(numberInput);
@@ -230,10 +315,60 @@ const addCart = item => {
     portfoliaMenu.appendChild(totalMenu);
     portfoliaMenu.appendChild(priceMenu);
 
+    plusBtn.addEventListener("click", e => updateNumberItem(item, 1));
+    abBtn.addEventListener("click", e => updateNumberItem(item, -1));
+
+    // portfoliaMenu.classList.add('animation-item-box');
+
+    //saler
+    const proName = document.createElement("h2");
+    proName.textContent = item.name;
+    const decriPro = document.createElement("span");
+    decriPro.textContent = item.describe;
+    const numberPro = document.createElement("span");
+    numberPro.setAttribute("id", "number" + item.id);
+    numberPro.textContent = item.number;
+    const totalPro = document.createElement("span");
+    totalPro.textContent = "Total";
+    const pricePro = document.createElement("span");
+    pricePro.setAttribute("id", "pricePro" + item.id);
+    pricePro.textContent = item.price.sellPrice;
+
+    const itemSaler = document.createElement("div");
+    itemSaler.classList.add("item-box");
+    itemSaler.setAttribute("id", "itemSaler" + item.id);
+
+    listCartSaler.appendChild(itemSaler);
+
+    itemSaler.appendChild(proName);
+    itemSaler.appendChild(decriPro);
+    itemSaler.appendChild(numberPro);
+    itemSaler.appendChild(totalPro);
+    itemSaler.appendChild(pricePro);
   }
 
-
- 
+  updatePriceItem(item);
 };
 
 initItems();
+
+const displayMenu = (close) =>{
+  if (menu.classList.contains("menu-show")) {
+    menu.classList.remove("menu-show");
+  }
+  else if(close === false)
+    menu.classList.add("menu-show");
+
+  if(nav.classList.contains('nav-icon') && !close ){
+    nav.classList.remove('nav-icon');
+    nav.classList.add('nav-icon-close')
+  }
+  else{
+    nav.classList.add('nav-icon');
+    nav.classList.remove('nav-icon-close')
+  }
+}
+
+
+nav.addEventListener("click",e=>displayMenu(false));  
+mainContent.addEventListener("click",e=>displayMenu(true));
